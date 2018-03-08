a81c0589f7fe1d669d406d46f2484bdbe14ceb82
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-08 11:04:41 -0600

    Tests

---
+++ test/job_test.js
@@ -0,0 +1,26 @@
+require('./test_helper')
+
+const Job = require('../lib/job');
+
+describe('Job', () => {
+  let date = new Date();
+
+  let job = new Job(() => {
+    return Math.pow(2,2);
+  }, date);
+
+  it('should be able to create a job', () => {
+    expect(job).to.be.an.instanceOf(Job);
+  });
+
+  it('should have a method and a date', () => {
+    expect(job.method).to.be.an.instanceOf(Function);
+    expect(job.date).to.be.an.instanceOf(Date);
+
+    expect(job.date).to.equal(date);
+  });
+
+  it('should be able to run method', () => {
+    expect(job.execute).to.equal(4);
+  });
+});
---
+++ test/scheduler_test.js
@@ -0,0 +1,62 @@
+require('./test_helper');
+
+const path = require('path');
+const fs = require('fs');
+
+const Scheduler = require('../lib/scheduler');
+const Job = require('../lib/job');
+
+
+describe('Scheduler', () => {
+  let dirPath = path.join(__dirname, '../tmp');
+  let filePath = path.join(dirPath, 'schedule.json');
+  let tempFilePath = path.join(dirPath, 'temp_schedule.json');
+
+  let scheduler = null;
+
+  before(() => {
+      fs.mkdirSync(dirPath);
+      fs.access(dirPath, err => {
+        expect(!!err).to.be.false;
+      });
+
+      scheduler = new Scheduler(filePath);
+  });
+
+  it('should start with an empty database', () => {
+    let _scheduler = new Scheduler(tempFilePath);
+    expect(_scheduler.schedule.length).to.equal(0);
+  });
+
+  it('should be able to schedule jobs', () => {
+    let job = scheduler.add(() => {}, new Date());
+
+    expect(job).to.be.an.instanceOf(Job);
+    expect(scheduler.schedule.length).to.equal(1);
+
+    let job2 = scheduler.add(() => {}, new Date());
+
+    expect(job2).to.be.an.instanceOf(Job);
+    expect(scheduler.schedule.length).to.equal(2);
+  });
+
+  it('should be able to load jobs', () => {
+    let _scheduler = new Scheduler(filePath);
+    expect(scheduler.schedule.length).to.equal(2);
+  });
+
+  afterEach(() => {
+    fs.access(tempFilePath, err => {
+      if(!err) {
+        fs.unlinkSync(tempFilePath);
+      }
+    });
+  });
+
+  after(() => {
+    fs.rmdirSync(dirPath);
+    fs.access(dirPath, err => {
+      expect(!!err).to.be.true;
+    });
+  });
+});
